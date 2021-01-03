1c69d3a5715afea8666b14e54d79e490c81f4175
kinga j <kingajaglo.99@gmail.com>
2021-01-03 20:31:05 +0100

    fix: create random ships

--- scripts/randomShips.js
+++ scripts/randomShips.js
@@ -1,14 +1,14 @@
 const board = []
 const size = 10;
-const boardSize = size+2;
+const boardSize = size + 2;
 
 function createEmptyBoardAndBorders() {
-    for(let row=0; row<boardSize; row++) {
+    for (let row = 0; row < boardSize; row++) {
         const boardRow = [];
-        for(let col=0; col<boardSize; col++) {
-            if(col == 0 || col == 11 || row == 0 || row == 11) {
+        for (let col = 0; col < boardSize; col++) {
+            if (col == 0 || col == 11 || row == 0 || row == 11) {
                 boardRow.push(9); // create border
-            } else { 
+            } else {
                 boardRow.push(0); // create board
             }
         }
@@ -17,8 +17,8 @@ function createEmptyBoardAndBorders() {
 }
 
 function printBoard() {
-    for(let row=0; row<boardSize; row++) {
-        for(let col=0; col<boardSize; col++) {
+    for (let row = 0; row < boardSize; row++) {
+        for (let col = 0; col < boardSize; col++) {
             document.getElementById("printingArea").innerHTML += board[row][col];
             document.getElementById("printingArea").innerHTML += " ";
         }
@@ -28,143 +28,174 @@ function printBoard() {
 
 }
 
-function randomParameters(){
-    const randomRow = Math.round(Math.random()*100) % 10+1;
-    const randomCol = Math.round(Math.random()*100) % 10+1;
+function randomParameters() {
+    const randomRow = Math.round(Math.random() * 100) % 10 + 1;
+    const randomCol = Math.round(Math.random() * 100) % 10 + 1;
 
-    let randomDirection = Math.round(Math.random()*10) % 2;
+    let randomDirection = Math.round(Math.random() * 10) % 2;
 
-    if(randomDirection==0){
+    if (randomDirection == 0) {
         randomDirection = "vertical";
     } else {
         randomDirection = "horizontal";
     }
-    console.log(randomRow);
-    console.log(randomCol);
-    console.log(randomDirection);
-    return {x:randomRow, y:randomCol, dir:randomDirection};
-    
+
+    return { x: randomRow, y: randomCol, dir: randomDirection };
+
 }
 
-function placeNear(params) {
-const dxdy = [{x:-1, y:0},{x:-1, y:1},{x:0, y:1},{x:1, y:1},{x:1, y:0},{x:1, y:-1},{x:0, y:-1},{x:-1, y:-1}];
-    for(item of dxdy){
-        if(board[params.x+item.x][params.y+item.y]==0){
-            board[params.x+item.x][params.y+item.y]=2;
+
+function checkNear(params, dxdyShip) {
+    around = 0;
+    const dxdy = [{ x: -1, y: 0 }, { x: -1, y: 1 }, { x: 0, y: 1 }, { x: 1, y: 1 }, { x: 1, y: 0 }, { x: 1, y: -1 }, { x: 0, y: -1 }, { x: -1, y: -1 }];
+    for (square of dxdyShip) {
+
+        for (item of dxdy) {
+            console.log("here", params.x + item.x + square.x, params.y + item.y + square.y)
+
+            if (board[params.x + item.x + square.x][params.y + item.y + square.y] == 0 || board[params.x + item.x + square.x][params.y + item.y + square.y] == 9) {
+                around += 1;
+            }
         }
-            
     }
+    return around;
 }
+
+
+
+// function placeNear(params, dxdyShip) {
+//     const dxdy = [{x:-1, y:0},{x:-1, y:1},{x:0, y:1},{x:1, y:1},{x:1, y:0},{x:1, y:-1},{x:0, y:-1},{x:-1, y:-1}];
+//     for(square of dxdyShip){
+
+//     for(item of dxdy){
+
+//             if(board[params.x+item.x+square.x][params.y+item.y+square.y]==0 ) {
+//                 board[params.x+item.x+square.x][params.y+item.y+square.y]=2;
+//             } 
+//         }
+//     }
+// }
+
+
 
 function placeShip(params, dxdy) {
-    for(item of dxdy){
-        board[params.x+item.x][params.y+item.y]=1;
+    for (item of dxdy) {
+        board[params.x + item.x][params.y + item.y] = 1;
     }
 }
 
 function sizeThatCanBePlaced(params, dxdy) {
-    let size=0;
-    for(item of dxdy){
-        if(board[params.x+item.x][params.y+item.y]==9){
+    let size = 0;
+    for (item of dxdy) {
+        if (board[params.x + item.x][params.y + item.y] == 9) {
             return size;
-        } else if (board[params.x+item.x][params.y+item.y]==0){
+        } else if (board[params.x + item.x][params.y + item.y] == 0) {
             size++;
         }
+
     }
-    return size;  
+    console.log("size placed: ", size)
+    return size;
 }
 
 function chooseShip(params, shipLength) {
+    console.log("len: ", shipLength)
 
-    const dxdy1 = [{x:0, y:0}]
+    const dxdy1 = [{ x: 0, y: 0 }]
 
-    const dxdy2h = [{x:0, y:0},{x:0, y:1}]
-    const dxdy2v = [{x:0, y:0},{x:1, y:0}]
+    const dxdy2h = [{ x: 0, y: 0 }, { x: 0, y: 1 }]
+    const dxdy2v = [{ x: 0, y: 0 }, { x: 1, y: 0 }]
 
-    const dxdy3h = [{x:0, y:0},{x:0, y:1},{x:0, y:2}]
-    const dxdy3v = [{x:0, y:0},{x:1, y:0},{x:2, y:0}]
+    const dxdy3h = [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }]
+    const dxdy3v = [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }]
 
-    const dxdy4h = [{x:0, y:0},{x:0, y:1},{x:0, y:2},{x:0, y:3}]
-    const dxdy4v = [{x:0, y:0},{x:1, y:0},{x:2, y:0},{x:3, y:0}]
+    const dxdy4h = [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }, { x: 0, y: 3 }]
+    const dxdy4v = [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }, { x: 3, y: 0 }]
 
-    switch(shipLength){
+    switch (shipLength) {
         case 1:
             return dxdy1;
             break;
 
         case 2:
-            switch(params.dir){
-                case "horizontal": 
+            switch (params.dir) {
+                case "horizontal":
                     return dxdy2h;
-                break;
-        
+                    break;
+
                 case "vertical":
                     return dxdy2v;
-                break;
+                    break;
             }
         case 3:
-            switch(params.dir){
-                case "horizontal": 
+            switch (params.dir) {
+                case "horizontal":
                     return dxdy3h;
-                break;
-        
+                    break;
+
                 case "vertical":
                     return dxdy3v;
-                break;
+                    break;
             }
         case 4:
-            switch(params.dir){
-                case "horizontal": 
+            switch (params.dir) {
+                case "horizontal":
                     return dxdy4h;
-                break;
-        
+                    break;
+
                 case "vertical":
                     return dxdy4v;
-                break;
+                    break;
             }
     }
-
+    console.log("ship:", dxdy.x);
+    return dxdy;
 }
 
 function createRandomShips() {
-    shipsPlaced=[0,0,0,0,0,0,0,0,0,0];
-    shipsLengths = [4,3,3,2,2,2,1,1,1,1];
+    shipsPlaced = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    shipsLengths = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
 
-    for(i in shipsPlaced) {
+    for (i in shipsPlaced) {
         //i=0;
-        console.log(shipsPlaced[i]);
+        //console.log(shipsPlaced[i]);
         console.log(shipsLengths[i]);
-    
-        while(shipsPlaced[i]==0) {
+
+        while (shipsPlaced[i] == 0) {
             const params = randomParameters();
-    
+
             console.log(params);
-        
+
             const dxdy = chooseShip(params, shipsLengths[i]);
             const sizeThatFits = sizeThatCanBePlaced(params, dxdy);
-    
-        
-            if(sizeThatFits==shipsLengths[i]){
-                shipsPlaced[i]=1;
-                console.log("Ship can be placed here");
-                placeShip(params,dxdy);
-                placeNear(params);
-                
-                
+
+
+            if (sizeThatFits == shipsLengths[i]) {
+                console.log("fit", sizeThatFits);
+                const near = checkNear(params, dxdy);
+                if (8 * shipsLengths[i] == near) {
+                    shipsPlaced[i] = 1;
+                    console.log("Ship can be placed here");
+                    placeShip(params, dxdy);
+                    //placeNear(params, dxdy);
+                    //printBoard();
+                }
+
             } else {
-                console.log("Ship cannot be placed here :( ");
+                console.log("Ship cannot be placed here");
             }
-            console.log(shipsPlaced[i]);
-    
+            //console.log(shipsPlaced[i]);
+
         }
     }
 }
 
 
 
-     
+
 
 
 createEmptyBoardAndBorders();
 createRandomShips();
 printBoard();
+
