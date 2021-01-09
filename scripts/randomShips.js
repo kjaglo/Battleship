fbedae1414b6182bc36aefa18177cb08664aebb3
kinga j <kingajaglo.99@gmail.com>
2021-01-10 00:57:29 +0100

    fix: clean code

--- scripts/randomShips.js
+++ scripts/randomShips.js
@@ -8,7 +8,27 @@ const sunkSquare = 3;
 const nearShipSquare = 4;
 const shipSquare = 5;
 
+const border = 9;
 
+let turn = "player";
+
+let board;
+let boardOponent;
+
+
+let shipsToFind;
+let shipsToFindO;
+let squaresO;
+
+let squares;
+
+let randomOrPlaceYourShips = 0;
+let currentSumShip = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let winningSumShip = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+
+let boardComputerShoot = createEmptyBoardAndBorders();
+let leftShips = getLeftShips(".ships-left");
+let leftShipsOponent;
 function createEmptyBoardAndBorders() {
     let board = []
     // board:
@@ -23,8 +43,8 @@ function createEmptyBoardAndBorders() {
     for (let row = 0; row < boardSize; row++) {
         const boardRow = [];
         for (let col = 0; col < boardSize; col++) {
-            if (col == 0 || col == 11 || row == 0 || row == 11) {
-                boardRow.push(9); // create border
+            if (col === 0 || col === boardSize - 1 || row === 0 || row === boardSize - 1) {
+                boardRow.push(border); // create border
             } else {
                 boardRow.push(0); // create board
             }
@@ -33,17 +53,18 @@ function createEmptyBoardAndBorders() {
     }
     return board;
 }
+
 function clearPrintBoard() {
     if (document.getElementById("printingArea")) {
         document.getElementById("printingArea").remove();
     }
 }
+
 function printBoard(board) {
     clearPrintBoard();
 
-    let body = document.getElementsByTagName('body')[0];
-
-    let area = document.createElement('div');
+    const body = document.getElementsByTagName('body')[0];
+    const area = document.createElement('div');
     area.id = "printingArea";
 
     body.appendChild(area);
@@ -59,40 +80,33 @@ function printBoard(board) {
 }
 
 function randomParameters() {
+
     const randomRow = Math.round(Math.random() * 100) % 10 + 1;
     const randomCol = Math.round(Math.random() * 100) % 10 + 1;
-
-    let randomDirection = Math.round(Math.random() * 10) % 2;
-
-    if (randomDirection == 0) {
-        randomDirection = "vertical";
-    } else {
-        randomDirection = "horizontal";
-    }
+    const randomDirection = Math.round(Math.random() * 10) % 2 === 0 ? "vertical" : "horizontal";
 
     return { x: randomRow, y: randomCol, dir: randomDirection };
-
 }
 
+function getNearCount(board, params, dxdyShip) {
 
-
-function checkNear(board, params, dxdyShip) {
-    around = 0;
+    let around = 0;
     const dxdy = [{ x: -1, y: 0 }, { x: -1, y: 1 }, { x: 0, y: 1 }, { x: 1, y: 1 }, { x: 1, y: 0 }, { x: 1, y: -1 }, { x: 0, y: -1 }, { x: -1, y: -1 }];
+
     for (square of dxdyShip) {
-
         for (item of dxdy) {
-
-            if (board[params.x + item.x + square.x][params.y + item.y + square.y] == 0 || board[params.x + item.x + square.x][params.y + item.y + square.y] == 9) {
-                around += 1;
+            const row = params.x + item.x + square.x;
+            const col = params.y + item.y + square.y;
+            if (board[row][col] === 0 || board[row][col] === border) {
+                around++;
             }
         }
     }
     return around;
 }
 
-
 function placeShip(shipsToFind, board, params, dxdy) {
+
     const squares = [];
 
     for (item of dxdy) {
@@ -100,8 +114,6 @@ function placeShip(shipsToFind, board, params, dxdy) {
         squares.push([params.x + item.x - 1, params.y + item.y - 1])
     }
     shipsToFind.push([squares, Array(squares.length).fill(0)]);
-
-
     return shipsToFind;
 }
 
@@ -133,37 +145,30 @@ function chooseShip(params, shipLength) {
     switch (shipLength) {
         case 1:
             return dxdy1;
-            break;
 
         case 2:
             switch (params.dir) {
                 case "horizontal":
                     return dxdy2h;
-                    break;
 
                 case "vertical":
                     return dxdy2v;
-                    break;
             }
         case 3:
             switch (params.dir) {
                 case "horizontal":
                     return dxdy3h;
-                    break;
 
                 case "vertical":
                     return dxdy3v;
-                    break;
             }
         case 4:
             switch (params.dir) {
                 case "horizontal":
                     return dxdy4h;
-                    break;
 
                 case "vertical":
                     return dxdy4v;
-                    break;
             }
     }
     return dxdy;
@@ -176,34 +181,27 @@ function createRandomShips(board) {
 
     for (i in shipsPlaced) {
 
-        while (shipsPlaced[i] == 0) {
+        while (shipsPlaced[i] === 0) {
             const params = randomParameters();
-
-
             const dxdy = chooseShip(params, shipsLengths[i]);
             const sizeThatFits = sizeThatCanBePlaced(board, params, dxdy);
 
-
-            if (sizeThatFits == shipsLengths[i]) {
-                const near = checkNear(board, params, dxdy);
-                if (8 * shipsLengths[i] == near) {
+            if (sizeThatFits === shipsLengths[i]) {
+                const near = getNearCount(board, params, dxdy);
+                if (8 * shipsLengths[i] === near) {
                     shipsPlaced[i] = 1;
                     shipsToFind = placeShip(shipsToFind, board, params, dxdy);
-
                 }
-
-            } else {
             }
-
         }
     }
     return shipsToFind;
 }
 
-const getSquares = function (tableId) {
+function getSquares(tableId) {
 
-    let table = document.getElementById(tableId);
-    let squares = [];
+    const table = document.getElementById(tableId);
+    const squares = [];
 
     let k = 0;
     for (let i = 0; i < 10; i++) {
@@ -211,15 +209,13 @@ const getSquares = function (tableId) {
         for (let j = 0; j < 10; j++) {
             squaresRow.push(table.getElementsByTagName('TD')[k]);
             k++;
-
         }
         squares.push(squaresRow);
     }
     return squares;
 }
 
-
-const resetTexts = function (texts) {
+function resetTexts(texts) {
 
     for (let m = 0; m < 10; m++) {
         texts[m].className = "default-text";
@@ -228,49 +224,45 @@ const resetTexts = function (texts) {
     return texts;
 }
 
+function getLeftShips(shipsTypeClass) {
+    console.log(shipsTypeClass)
 
-
-
-const getLeftShips = function () {
-
-    let leftShips = [];
-
-    leftShips.push(document.querySelector("#s4"));
-    leftShips.push(document.querySelector("#s3a"));
-    leftShips.push(document.querySelector("#s3b"));
-    leftShips.push(document.querySelector("#s2a"));
-    leftShips.push(document.querySelector("#s2b"));
-    leftShips.push(document.querySelector("#s2c"));
-    leftShips.push(document.querySelector("#s1a"));
-    leftShips.push(document.querySelector("#s1b"));
-    leftShips.push(document.querySelector("#s1c"));
-    leftShips.push(document.querySelector("#s1d"));
-
+    const shipsType = document.querySelector(shipsTypeClass);
+    console.log(shipsType)
+    const leftShips = [];
+    let ids;
+    if(shipsTypeClass == ".left-ships") {
+        ids = ["#s4", "#s3a", "#s3b", "#s2a", "#s2b", "#s2c", "#s1a", "#s1b", "#s1c", "s1d"];
+    } else {
+        ids = ["#so4", "#so3a", "#so3b", "#so2a", "#so2b", "#so2c", "#so1a", "#so1b", "#so1c", "so1d"];
+    }
+    for(i in ids){
+        leftShips.push(shipsType.querySelector(ids[i]))
+    }
+    console.log(leftShips)
 
     return leftShips
 }
 
+// function getLeftShipsOponent() {
 
-const getLeftShipsOponent = function () {
+//     const leftShipsOponent = [];
 
-    let leftShipsOponent = [];
+//     leftShipsOponent.push(document.querySelector("#so4"));
+//     leftShipsOponent.push(document.querySelector("#so3a"));
+//     leftShipsOponent.push(document.querySelector("#so3b"));
+//     leftShipsOponent.push(document.querySelector("#so2a"));
+//     leftShipsOponent.push(document.querySelector("#so2b"));
+//     leftShipsOponent.push(document.querySelector("#so2c"));
+//     leftShipsOponent.push(document.querySelector("#so1a"));
+//     leftShipsOponent.push(document.querySelector("#so1b"));
+//     leftShipsOponent.push(document.querySelector("#so1c"));
+//     leftShipsOponent.push(document.querySelector("#so1d"));
 
-    leftShipsOponent.push(document.querySelector("#so4"));
-    leftShipsOponent.push(document.querySelector("#so3a"));
-    leftShipsOponent.push(document.querySelector("#so3b"));
-    leftShipsOponent.push(document.querySelector("#so2a"));
-    leftShipsOponent.push(document.querySelector("#so2b"));
-    leftShipsOponent.push(document.querySelector("#so2c"));
-    leftShipsOponent.push(document.querySelector("#so1a"));
-    leftShipsOponent.push(document.querySelector("#so1b"));
-    leftShipsOponent.push(document.querySelector("#so1c"));
-    leftShipsOponent.push(document.querySelector("#so1d"));
+//     return leftShipsOponent;
+// }
 
-
-    return leftShipsOponent;
-}
-
-const showShips = function (board, td, i, j) {
+function showShips(board, td, i, j) {
     i++;
     j++;
     switch (board[i - 1][j]) {
@@ -295,19 +287,18 @@ const showShips = function (board, td, i, j) {
     }
 }
 
-const dontShowShips = function (td) {
+function dontShowShips(td) {
     td.className = "default";
 }
 
-const drawTable = function (board, player) {
+function drawTable(board, player) {
 
-    let body = document.getElementById('container');
+    const body = document.getElementById('container');
     let table;
     if (player == "oponent") {
         table = document.getElementById('table-oponent');
         if (table) {
             table.remove();
-
         }
         table = document.createElement('table');
         table.id = "table-oponent";
@@ -315,28 +306,19 @@ const drawTable = function (board, player) {
         table = document.getElementById('table-player');
         if (table) {
             table.remove();
-
         }
         table = document.createElement('table');
         table.id = "table-player";
     }
 
-
-    //table.setAttribute('border', '1');
-
     let tbdy = document.createElement('tbody');
 
     for (let i = 0; i < 11; i++) {
 
-        let tr = document.createElement('tr');
+        const tr = document.createElement('tr');
+        const th = document.createElement('th');
 
-        let th = document.createElement('th');
-
-        if (i == 0) {
-            //th.appendChild(document.createTextNode('X'));
-        }
-
-        else {
+        if (i != 0) {
             th.appendChild(document.createTextNode(String.fromCharCode(65 + i - 1)));// Rownames: ASCII A-J
         }
 
@@ -345,30 +327,23 @@ const drawTable = function (board, player) {
         for (let j = 0; j < 10; j++) {
 
             if (i == 0) {
-                let th = document.createElement('th');
+                const th = document.createElement('th');
                 th.appendChild(document.createTextNode(j + 1)); // Colnames: 1-10
                 tr.appendChild(th);
             }
 
             else {
-                let td = document.createElement('td');
-
+                const td = document.createElement('td');
                 //td.appendChild(document.createTextNode("i"+(i-1)+" j"+j)); 
-
                 showShips(board, td, i, j);
                 //dontShowShips(td);
-
                 tr.appendChild(td);
             }
         }
-
         tbdy.appendChild(tr);
     }
-
     table.appendChild(tbdy);
-
     body.appendChild(table)
-
 }
 
 function currentHitSumAll(shipsToFind, currentSumShip, winningSumShip) {
@@ -378,7 +353,6 @@ function currentHitSumAll(shipsToFind, currentSumShip, winningSumShip) {
         }
     }
     return true;
-
 }
 
 function currentHitSumShip(shipsToFind, i) {
@@ -396,7 +370,6 @@ function sunkShip(shipsToFind, squares, i) {
         const col = shipsToFind[i][0][k][1];
 
         squares[row][col].className = "hit-and-sunk"
-
     }
 }
 
@@ -405,7 +378,8 @@ function winMessageClear() {
         document.getElementById("won").remove();
     }
 }
-const winMessage = function (player) {
+
+function winMessage(player) {
 
     let body = document.getElementsByTagName('body')[0];
     let div = document.createElement('div');
@@ -417,10 +391,8 @@ const winMessage = function (player) {
     }
     body.appendChild(div);
 }
-let turn = "player";
 
 function hitShip(player, board, shipsToFind, leftShips, squares, I, J, currentSumShip, winningSumShip) {
-
 
     const row = I;
     const col = J;
@@ -437,8 +409,8 @@ function hitShip(player, board, shipsToFind, leftShips, squares, I, J, currentSu
 
                     if (currentHitSumShip(shipsToFind, i) == shipsToFind[i][j].length) {
                         sunkShip(shipsToFind, squares, i);
+                        console.log(i, leftShips[i])
                         leftShips[i].className += " ship-left-sunked";
-
                     }
                     if (currentHitSumAll(shipsToFind, currentSumShip, winningSumShip)) {
                         winMessage(player);
@@ -447,8 +419,8 @@ function hitShip(player, board, shipsToFind, leftShips, squares, I, J, currentSu
             }
         }
     }
-
 }
+
 function hit(player, board, shipsToFind, leftShips, squares, I, J, currentSumShip, winningSumShip) {
     console.log("sq", squares[I][J].className);
     if (squares[I][J].className == "ship") {
@@ -467,7 +439,7 @@ const missShip = function (board, squares, i, j) {
     board[i + 1][j + 1] = missedSquare;
 
 }
-const miss = function (board, squares, i, j) {
+function miss (board, squares, i, j) {
     if (squares[i][j].className == "default") {
         if (turn == "player") {
             console.log("pl", turn);
@@ -478,7 +450,7 @@ const miss = function (board, squares, i, j) {
     }
 }
 
-const playGuess = function (player, board, shipsToFind, squares, leftShips, texts) {
+function playGuess(player, board, shipsToFind, squares, leftShips, texts) {
 
     let currentSumShip = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     let winningSumShip = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
@@ -495,37 +467,14 @@ const playGuess = function (player, board, shipsToFind, squares, leftShips, text
             }
         }
     }
-
-
 }
-
-
-let board;
-let boardOponent;
-let boardComputerShoot = createEmptyBoardAndBorders();
-
-let shipsToFind;
-let shipsToFindO;
-let squaresO;
-let leftShips = getLeftShips();
-let leftShipsOponent = getLeftShipsOponent();
-
-let squares;
-
-let randomOrPlaceYourShips = 0;
-let currentSumShip = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let winningSumShip = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
 
 function aroundShoot(player, board, shoot) {
     let randomDirection = Math.round(Math.random() * 10) % 4;
-
-    let around=[{ x: -1, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 0 }, { x: 0, y: -1 }];
-
-
+    let around = [{ x: -1, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 0 }, { x: 0, y: -1 }];
 }
+
 function randomSquareShoot(player, board, shoot) {
-
-
     let randomSquare;
     let squareShooted = 0;
     while (squareShooted == 0 && shoot <= 100 && turn == "oponent") {
@@ -540,7 +489,6 @@ function randomSquareShoot(player, board, shoot) {
                 missShip(board, squares, randomRow - 1, randomCol - 1);
                 turn = "player";
             }
-
         } else if (board[randomRow][randomCol] == 5) {
             if (turn == "oponent") {
                 console.log("opo", turn);
@@ -549,83 +497,58 @@ function randomSquareShoot(player, board, shoot) {
                 hitShip(player, board, shipsToFind, leftShips, squares, randomRow - 1, randomCol - 1, currentSumShip, winningSumShip);
                 turn = "player";
             }
-
         } else {
-
             console.log("the same shoot");
-
         }
-
-
     }
-
     board[0][0] = shoot;
-
     //printBoard(board);
     return randomSquare;
-
 }
+
 let shoot = 0;
 function shootOnClick() {
     //document.getElementsByName("body").innerHTML+=shoot;
-
-
     shoot++;
     const randomSquare = randomSquareShoot("oponent", board, shoot);
     if (!randomSquare) {
         shoot--;
         alert("its your turn");
-
     }
     if (shoot == 100) {
         document.querySelector("#shoot-btn").className = "disappeared";
-
     }
     turn = "player"
-
 }
-
 
 function randomShipsOnClick() {
     winMessageClear();
     board = createEmptyBoardAndBorders();
     turn = "player"
     shipsToFind = createRandomShips(board);
-    //createAndShowNear();
-    //printBoard(board);
-
     drawTable(board, "player");
     randomOrPlaceYourShips = 1;
     squares = getSquares("table-player");
 }
 
-
-
 board = createEmptyBoardAndBorders();
 drawTable(board, "player");
-
-//console.log("ships", getLeftShips());
-
-
 
 function resetShipsOnClick() {
     let board = createEmptyBoardAndBorders();
     randomOrPlaceYourShips = 0;
 
     drawTable(board, "player");
-
 }
 
-
-function buttonsChanges(play) {
-    if (play == true) {
+function changeButtons(play) {
+    if (play) {
         document.querySelector("#random-btn").className = "";
         document.querySelector("#reset-btn").className = "";
         document.querySelector("#play-btn").className = "";
 
         document.querySelector(".ships-left-oponent").className = "ships-left-oponent disappeared";
         document.querySelector("#shoot-btn").className = "disappeared";
-
         document.querySelector("#quit-btn").className = "disappeared";
     } else {
         document.querySelector("#random-btn").className = "disappeared";
@@ -633,15 +556,9 @@ function buttonsChanges(play) {
         document.querySelector("#play-btn").className = "disappeared";
 
         document.querySelector(".ships-left-oponent").className = "ships-left-oponent";
-
         document.querySelector("#quit-btn").className = "";
         document.querySelector("#shoot-btn").className = "";
-
     }
-
-
-    //document.querySelector("#play-btn").className = "";
-
 }
 
 function randomOponentShips() {
@@ -650,34 +567,29 @@ function randomOponentShips() {
     shipsToFindO = createRandomShips(boardOponent);
     drawTable(boardOponent, "oponent");
     squaresO = getSquares("table-oponent");
-
-    //textsO = resetTexts(tO);
 }
-
 
 function playOnClick() {
 
     if (randomOrPlaceYourShips == 1) {
         randomOrPlaceYourShips = 0;
-        buttonsChanges(false);
+        changeButtons(false);
         randomOponentShips();
+        leftShipsOponent = getLeftShips(".ships-left-oponent");
         playGuess("player", boardOponent, shipsToFindO, squaresO, leftShipsOponent);
     }
     else {
         alert("Place or random your ships");
     }
-
 }
 
 function quitOnClick() {
 
-    let answer = confirm("Are you sure you want to end game?");
+    const answer = confirm("Are you sure you want to end game?");
     console.log(answer);
-    if (answer == true) {
-        buttonsChanges(true);
-
+    if (answer) {
+        changeButtons(true);
         resetShipsOnClick();
-        //randomOponentShips();
         document.getElementById("table-oponent").remove();
     }
 }
